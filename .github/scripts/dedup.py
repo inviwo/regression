44e2e1336db832f2b30ca96274ee22978a8d437d
Peter Steneteg <peter@steneteg.se>
2025-02-07 19:26:10 +0100

    More logging

--- .github/scripts/dedup.py
+++ .github/scripts/dedup.py
@@ -28,13 +28,13 @@ def replaceSource(items):
         src = item["src"]
         if reportDir / src in orgMap:
             org = orgMap[reportDir/src]
-            #rel = os.path.relpath(org, (reportDir/src).parent)
             item["src"] = f"/regression/{org}"
 
 
 root = pathlib.Path(sys.argv[2])
 reports = list(root.glob("**/report*.html"))
 
+print("::group::Process reports", flush=True)
 for report in reports:
     print(f"Processing {report}", flush=True)
     reportDir = report.parent
@@ -48,23 +48,14 @@ for report in reports:
 
     with open(report, 'w') as f:
         f.write(str(soup))
+print("::endgroup::", flush=True)
 
-
-print(f"Removing duplicates", flush=True)
+print(f"::group::Removing duplicates", flush=True)
 for (first, *rest) in duplicates.values():
     if first.suffix in [".png", ".js"]:
         for item in rest:
             item.unlink()
 
         if len(rest) > 0:
-            print(f"Removed {len(rest)} duplicates of {first}")
-
-if False:
-    for (first, *rest) in duplicates.values():
-        for item in rest:
-            item.unlink()
-            relpath = pathlib.Path(os.path.relpath(first, item.parent))
-            item.symlink_to(relpath)
-
-        if len(rest) > 0:
-            print(f"Created {len(rest)} symlinks to {first}")
+            print(f"Removed {len(rest)} duplicates of {first}", flush=True)
+print("::endgroup::", flush=True)
