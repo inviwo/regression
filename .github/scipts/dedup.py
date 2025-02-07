7d135624aff078c4a78f6e194eda46b9dca3c7ac
Peter Steneteg <peter@steneteg.se>
2025-02-07 14:31:55 +0100

    Try and deduplicate files

---
+++ .github/scipts/dedup.py
@@ -0,0 +1,17 @@
+import pathlib
+import sys
+
+lines = open(sys.argv[1]).readlines()
+
+duplicates = {}
+for line in lines:
+    (_, _, sha, path) = line.split()
+    if sha in d:
+        duplicates[sha].append(pathlib.Path(path))
+    else:
+        duplicates[sha] = [pathlib.Path(path)]
+
+for (first, *rest) in duplicates.values():
+    if len(rest) > 0:
+        rest.unlink()
+        rest.symlink_to(first)
