73d3064e9348505b38bbc67e483fea08863bec75
Peter Steneteg <peter@steneteg.se>
2026-02-11 08:27:49 +0100

    Debugging

--- .github/scripts/dedup.py
+++ .github/scripts/dedup.py
@@ -4,37 +4,31 @@ import os
 
 from bs4 import BeautifulSoup
 
-lines = open(sys.argv[1]).readlines()
 
-print(f"Deduplicating files: {len(lines)}", flush=True)
+def findDuplicates(lines):
+    duplicates = {}
+    for line in lines:
+        try:
+            (_, _, sha, path) = line.split(maxsplit=3)
+            if sha in duplicates:
+                duplicates[sha].append(pathlib.Path(path))
+            else:
+                duplicates[sha] = [pathlib.Path(path)]
+        except Exception as e:
+            print(f"Error processing line: {line}", flush=True)
+            print(e, flush=True)
+    return duplicates
 
-duplicates = {}
-for line in lines:
-    try:
-        (_, _, sha, path) = line.split(maxsplit=3)
-        if sha in duplicates:
-            duplicates[sha].append(pathlib.Path(path))
-        else:
-            duplicates[sha] = [pathlib.Path(path)]
-    except Exception as e:
-        print(f"Error processing line: {line}", flush=True)
-        print(e, flush=True)
 
-print(f"Found {len(duplicates)} unique files out of {len(lines)} files", flush=True)
+def fileToOrg(duplicates):
+    orgMap = {}
+    for (first, *rest) in duplicates.values():
+        for item in rest:
+            orgMap[item] = first
+    return orgMap
 
-for key, value in duplicates.items():
-    print(key)
-    for v in value:
-        print(f"   {v}")
 
-exit(1)
-
-orgMap = {}
-for (first, *rest) in duplicates.values():
-    for item in rest:
-        orgMap[item] = first
-
-def replaceSource(items):
+def replaceSource(items, orgMap, reportDir):
     for item in items:
         src = item["src"]
         if reportDir / src in orgMap:
@@ -42,31 +36,55 @@ def replaceSource(items):
             item["src"] = f"/regression/{org}"
 
 
-root = pathlib.Path(sys.argv[2])
-reports = list(root.glob("**/report*.html"))
-
-print("::group::Process reports", flush=True)
-for (i, report) in enumerate(reports):
-    print(f"({i}/{len(reports)}) Processing {report}", flush=True)
+def processReport(report:pathlib.Path, orgMap):
     reportDir = report.parent
 
     with open(report) as f:
         html = f.read()
+
     soup = BeautifulSoup(html, 'html.parser')
 
-    replaceSource(soup.find_all('img'))
-    replaceSource(soup.find_all('script'))
+    replaceSource(soup.find_all('img'), orgMap, reportDir)
+    replaceSource(soup.find_all('script'), orgMap, reportDir)
 
     with open(report, 'w') as f:
         f.write(str(soup))
-print("::endgroup::", flush=True)
 
-print(f"::group::Removing duplicates", flush=True)
-for (first, *rest) in duplicates.values():
-    if first.suffix in [".png", ".js"]:
-        for item in rest:
-            item.unlink()
 
-        if len(rest) > 0:
-            print(f"Removed {len(rest)} duplicates of {first}", flush=True)
-print("::endgroup::", flush=True)
+def deleteDuplicates(duplicates):
+    for (first, *rest) in duplicates.values():
+        print(f"First: {first} {rest}")
+        if first.suffix in [".png", ".js"]:
+            for item in rest:
+                print(f"remove {item}")
+                item.unlink()
+
+            if len(rest) > 0:
+                print(f"Removed {len(rest)} duplicates of {first}", flush=True)
+
+
+if __name__ == "__main__":
+    print(f"Deduplicating", flush=True)
+
+    root = pathlib.Path(sys.argv[2])
+    lines = open(sys.argv[1]).readlines()
+
+    print(f"Found: {len(lines)} files", flush=True)
+
+    duplicates = findDuplicates(lines)
+
+    print(f"Found {len(duplicates)} unique files", flush=True)
+
+    orgMap = fileToOrg(duplicates)
+
+    reports = list(root.glob("**/report*.html"))
+
+    print("::group::Process reports", flush=True)
+    for (i, report) in enumerate(reports):
+        print(f"({i}/{len(reports)}) Processing {report}", flush=True)
+        processReport(report, orgMap)
+    print("::endgroup::", flush=True)
+
+    print(f"::group::Removing duplicates", flush=True)
+    deleteDuplicates(duplicates)
+    print("::endgroup::", flush=True)
