697773671e01db426614e2516b7d22effb54f32d
Peter Steneteg <peter@steneteg.se>
2026-04-01 09:46:28 +0200

    Added a file mapping to the josn reports in the dedup step

--- .github/scripts/dedup.py
+++ .github/scripts/dedup.py
@@ -1,6 +1,7 @@
 import pathlib
 import sys
 import os
+import json
 
 from bs4 import BeautifulSoup
 
@@ -11,9 +12,9 @@ def findDuplicates(lines):
         try:
             (_, _, sha, path) = line.strip().split(maxsplit=3)
             if sha in duplicates:
-                duplicates[sha].append(pathlib.Path(path))
+                duplicates[sha].append(path)
             else:
-                duplicates[sha] = [pathlib.Path(path)]
+                duplicates[sha] = [path]
         except Exception as e:
             print(f"Error processing line: {line}", flush=True)
             print(e, flush=True)
@@ -31,15 +32,15 @@ def fileToOrg(duplicates):
 def replaceSource(items, orgMap, reportDir):
     for item in items:
         src = item["src"]
-        if reportDir / src in orgMap:
-            org = orgMap[reportDir/src]
-            item["src"] = f"/regression/{org}"
+        key = f"{reportDir}/{src}"
+        if key in orgMap:
+            item["src"] = f"/regression/{orgMap[key]}"
 
 
-def processReport(report:pathlib.Path, orgMap):
-    reportDir = report.parent
+def processReport(reportFile:pathlib.Path, orgMap):
+    reportDir = reportFile.parent
 
-    with open(report) as f:
+    with open(reportFile) as f:
         html = f.read()
 
     soup = BeautifulSoup(html, 'html.parser')
@@ -47,15 +48,47 @@ def processReport(report:pathlib.Path, orgMap):
     replaceSource(soup.find_all('img'), orgMap, reportDir)
     replaceSource(soup.find_all('script'), orgMap, reportDir)
 
-    with open(report, 'w') as f:
+    with open(reportFile, 'w') as f:
         f.write(str(soup))
 
 
-def deleteDuplicates(duplicates):
+def processJsons(reportFile:pathlib.Path, orgMap):
+    reportDir = reportFile.parent
+
+    # read json from report
+    with open(reportFile) as f:
+        report = json.load(f)
+
+    for name, result in report.items():
+        [_, testDir] = result['outputdir'].split(f"/{name}/")
+       
+        updated = {}
+        for refImg in result['images']['refs']:
+            key = f"{reportDir.as_posix()}/{name}/{testDir}/imgref/{refImg}"
+            if key in orgMap:
+                updated[refImg] = f"https://inviwo.org/regression/{orgMap[key]}"
+            else:
+                updated[refImg] = f"https://inviwo.org/regression/{key}"
+        result['images']['refs-map'] = updated
+                      
+        updated = {}
+        for testImg in result['images']['imgs']:
+            key = f"{reportDir.as_posix()}/{name}/{testDir}/imgtest/{testImg}"
+            if key in orgMap:
+                updated[testImg] = f"https://inviwo.org/regression/{orgMap[key]}"
+            else:
+                updated[testImg] = f"https://inviwo.org/regression/{key}"
+        result['images']['imgs-map'] = updated
+
+    with open(reportFile, 'w') as f:
+        json.dump(report, f, indent=4)
+
+
+def deleteDuplicates(duplicates : dict[str, list[str]]):
     for (first, *rest) in duplicates.values():
-        if first.suffix in [".png", ".js"]:
+        if first.endswith((".png", ".js")):
             for item in rest:
-                item.unlink()
+                pathlib.Path(item).unlink()
 
             if len(rest) > 0:
                 print(f"Removed {len(rest)} duplicates of {first}", flush=True)
@@ -75,13 +108,21 @@ if __name__ == "__main__":
 
     orgMap = fileToOrg(duplicates)
 
-    reports = list(root.glob("**/report*.html"))
-
-    print("::group::Process reports", flush=True)
-    for (i, report) in enumerate(reports):
-        print(f"({i}/{len(reports)}) Processing {report}", flush=True)
-        processReport(report, orgMap)
+    jsonReports = list(root.glob("**/report.json"))
+    print("::group::Process json reports", flush=True)
+    for (i, jsonReport) in enumerate(jsonReports):
+        print(f"({i}/{len(jsonReports)}) Processing {jsonReport}", flush=True)
+        processJsons(jsonReport, orgMap)
     print("::endgroup::", flush=True)
+
+    htmlReports = list(root.glob("**/report*.html"))
+
+    print("::group::Process html reports", flush=True)
+    for (i, htmlReport) in enumerate(htmlReports):
+        print(f"({i}/{len(htmlReports)}) Processing {htmlReport}", flush=True)
+        processReport(htmlReport, orgMap)
+    print("::endgroup::", flush=True)
+
 
     print(f"::group::Removing duplicates", flush=True)
     deleteDuplicates(duplicates)
